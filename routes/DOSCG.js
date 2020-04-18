336f1297ed3a24260a1f87b2937d0bbc0ff335a9
warintiras <warintiras@gmail.com>
2020-04-18 10:43:37 +0700

    Work on webhook -v1

--- routes/DOSCG.js
+++ routes/DOSCG.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const abc  = require('../models/DOSCG');
 
 const app = express();
+const port = process.env.PORT || 4000;
+
 
 app.use(express.json()); // express.json returns a middleware
 
@@ -120,8 +122,45 @@ router.get('/api/abc', (req, res) => {
 
 router.post('/linebot/webhook', (req, res) => {
         // res.send("Hello Webhook!");
-        res.sendStatus(200);
-    });
+        // res.sendStatus(200);
+    // });
+
+
+    // app.post('/webhook', (req, res) => {
+        let reply_token = req.body.events[0].replyToken
+        reply(reply_token)
+        res.sendStatus(200)
+    })
+    app.listen(port)
+    function reply(reply_token) {
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {cyNaR/BGrcuuc/AWK9TGrZ0nHbZN2nBOIAbVmn6TsbMIVZQzm+u1swGWFr8UpKVNhF2GIhYWAKI0TQi7CjYYM//R08r2iuPiI/5nkMxWOXf7OG+WaVyIC/1tpJuwz6eemIoQryMErjz42XwGAi/KSQdB04t89/1O/w1cDnyilFU=}'
+        }
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: 'Hello'
+            },
+            {
+                type: 'text',
+                text: 'How are you?'
+            }]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+    }
+
+
+
+
+
     
 
 // router.post('/', (req, res) => {
